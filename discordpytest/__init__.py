c327cc547fdc561b43036216f47fa0d06311eb9d
mat <27899617+mat-1@users.noreply.github.com>
2020-10-14 11:24:22 -0500

    remove log

--- discordpytest/__init__.py
+++ discordpytest/__init__.py
@@ -1,12 +1,5 @@
 import discord
 import asyncio
-import logging
-
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
 
 
 class FakeClient(discord.Client):
