620c57d6731c22c423af7508ff641bd717c9bb04
mat <27899617+mat-1@users.noreply.github.com>
2020-10-15 16:24:11 -0500

    remove unnecessary prints

--- hypixel.py
+++ hypixel.py
@@ -18,11 +18,9 @@ async def get_user_data(ign):
 	'Returns the Discord username of a Hypixel IGN'
 	key = random.choice(api_keys)
 	url = f'https://skyblock-api.matdoes.dev/player/{ign}?key={key}&profiles=false'
-	print(url)
 	async with s.get(url) as r:
 		data = await r.json()
 	if data.get('error'):
-		print(data)
 		raise PlayerNotFound(f'Invalid Hypixel player: {ign}')
 	return data
 
@@ -31,11 +29,9 @@ async def get_hypixel_rank(ign):
 	'Returns the Hypixel rank of an IGN'
 	key = random.choice(api_keys)
 	url = f'https://api.hypixel.net/player?key={key}&name={ign}'
-	print(url)
 	async with s.get(url) as r:
 		data = await r.json()
 	if data.get('error'):
-		print(data)
 		raise PlayerNotFound(f'Invalid Hypixel player: {ign}')
 	rank_name = data.get('rank') or 'NON'
 	rank_name = rank_name.replace('_PLUS', '+')
