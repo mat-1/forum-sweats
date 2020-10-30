277505e1a1f9346d4a030a5f3c476ce061485576
mat <27899617+mat-1@users.noreply.github.com>
2020-10-30 13:34:36 -0500

    Create bobuxleaderboard.py

---
+++ bot/commands/bobuxleaderboard.py
@@ -0,0 +1,11 @@
+from ..betterbot import Member
+import bot.discordbot as discordbot
+import discord
+import db
+
+name = 'bobuxleaderboard'
+aliases = ['leaderboard', 'lb', 'bobuxlb', 'leaderboardbobux', 'lbbobux', 'blb']
+
+
+async def run(message, member: Member = None):
+	pass
