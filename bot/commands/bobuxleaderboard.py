1c4eb7e58d07e082cc97a969c2365a10f5969a92
mat <27899617+mat-1@users.noreply.github.com>
2020-10-30 14:19:23 -0500

    bobux leaderboard

--- bot/commands/bobuxleaderboard.py
+++ bot/commands/bobuxleaderboard.py
@@ -7,5 +7,20 @@ name = 'bobuxleaderboard'
 aliases = ['leaderboard', 'lb', 'bobuxlb', 'leaderboardbobux', 'lbbobux', 'blb']
 
 
-async def run(message, member: Member = None):
-	pass
+async def run(message):
+	print('doing bobux leaderboard')
+	leaderboard_raw = await db.get_bobux_leaderboard()
+	print('doing bobux leaderboard2')
+	leaderboard_strings = []
+	for position_0, member in enumerate(leaderboard_raw):
+		print('doing bobux leaderboard3')
+		position = position_0 + 1
+		member_id = member['discord']
+		bobux = member['bobux']
+		leaderboard_strings.append(f'{position}) <@{member_id}> (**{bobux}** bobux)')
+	print(leaderboard_strings)
+	embed = discord.Embed(
+		title='Bobux Leaderboard',
+		description='\n'.join(leaderboard_strings)
+	)
+	await message.channel.send(embed=embed)
