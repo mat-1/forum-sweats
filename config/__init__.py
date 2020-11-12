5f04a82ce2641f60926674837947a147d3a45462
mat <27899617+mat-1@users.noreply.github.com>
2020-11-12 15:21:11 -0600

    add bot.json

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,11 @@
 import json
 
-with open('config/roles.json', 'r') as f:
-	roles = json.loads(f.read())
+
+def read_config_file(name):
+	'Reads a json file from config/{name}.json'
+	with open(f'config/{name}.json', 'r') as f:
+		config_data = json.loads(f.read())
+	return config_data
+
+
+roles = read_config_file('roles')
