6ae7bd589a872c0b0336c0eba4f563480784b343
mat <27899617+mat-1@users.noreply.github.com>
2020-11-22 01:08:57 -0600

    start implementing sub gui

--- bot/commands/sub.py
+++ bot/commands/sub.py
@@ -1,14 +1,41 @@
-from urllib.parse import quote_plus
 from ..betterbot import Member
 import discord
-import random
-import json
 
 name = 'sub'
 aliases = ['simp', 'bobuxsub', 'subscribe', 'bobuxsubscribe', 'bobuxsimp']
 
 
-async def run(message, member: Member = None):
+tiers = {
+	't1': 20,
+	't2': 80,
+	't3': 200
+}
+
+agree_reaction = '✅'
+disagree_reaction = '❌'
+
+
+async def run(message, member: Member = None, tier: str = None):
 	if not member:
 		return await message.channel.send('Invalid member.')
-	await message.channel.send('ok')
+	if not tier:
+		return await message.channel.send('No tier specified (must be t1-t3).')
+	tier = tier.lower()
+	if tier not in tiers:
+		return await message.channel.send('Invalid tier (must be t1-t3).')
+
+	tier_cost = tiers[tier]
+
+	verify_message = await message.channel.send(embed=discord.Embed(
+		description=f'Are you sure you want to **{tier}** sub to {member.mention} by sending **{tier_cost}** bobux per week?'
+	))
+	await verify_message.add_reaction(agree_reaction)
+	await verify_message.add_reaction(disagree_reaction)
+
+	def check(reaction, user):
+		if user != message.author: return False
+		if reaction.emoji != agree_reaction and reaction.emoji != disagree_reaction: return False
+		if reaction.message != verify_message: return False
+		return True
+	reaction, user = await message.client.wait_for('reaction_add', check=check)
+	print(reaction, user)
