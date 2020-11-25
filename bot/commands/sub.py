618814284294e51653795074912e32cc03fea223
mat <27899617+mat-1@users.noreply.github.com>
2020-11-25 14:17:27 -0600

    check if user has enough bobux

--- bot/commands/sub.py
+++ bot/commands/sub.py
@@ -1,6 +1,7 @@
 from bot.betterbot import Member
 from bot import confirmgui
 import discord
+import db
 
 name = 'sub'
 aliases = ['simp', 'bobuxsub', 'subscribe', 'bobuxsubscribe', 'bobuxsimp']
@@ -13,6 +14,13 @@ tiers = {
 }
 
 
+async def verify_required_bobux(member, tier):
+	tier_cost = tiers[tier]
+	bobux = await db.get_bobux(member.id)
+	# If you have at least the required amount of bobux to sub for a week,
+	return bobux >= tier_cost
+
+
 async def run(message, member: Member = None, tier: str = None):
 	if not member:
 		return await message.channel.send('Invalid member.')
@@ -22,13 +30,20 @@ async def run(message, member: Member = None, tier: str = None):
 	if tier not in tiers:
 		return await message.channel.send('Invalid tier (must be t1-t3).')
 
+	if not await verify_required_bobux(message.author, tier):
+		return await message.channel.send(f'You don\'t have enough bobux to {tier} sub')
+
 	tier_cost = tiers[tier]
 
 	verify_message = await message.channel.send(embed=discord.Embed(
 		description=f'Are you sure you want to **{tier}** sub to {member.mention} by sending **{tier_cost}** bobux per week?'
 	))
-	print('ok making gui')
 	confirmed = await confirmgui.make_confirmation_gui(message.client, verify_message, message.author)
+
+	if not await verify_required_bobux(message.author, tier):
+		# Check again in case they sent bobux while the confirmation was active or something
+		return await message.channel.send(f'You don\'t have enough bobux to {tier} sub')
+
 
 	if confirmed:
 		await verify_message.edit(embed=discord.Embed(
