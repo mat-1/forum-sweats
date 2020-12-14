091fe272001473ad181d6efceb8348342283ecd2
mat <27899617+mat-1@users.noreply.github.com>
2020-12-14 17:26:49 -0600

    fix antisynths regex

--- modbot.py
+++ modbot.py
@@ -207,7 +207,7 @@ async def process_messsage(message, warn=True):
 		)
 		return
 
-	if re.search(r'((?:2[0-4]\d)|(?:25[0-5])|(?:1?\d?\d)).((?:2[0-4]\d)|(?:25[0-5])|(?:1?\d?\d)).((?:2[0-4]\d)|(?:25[0-5])|(?:1?\d?\d)).((?:2[0-4]\d)|(?:25[0-5])|(?:1?\d?\d))', content):
+	if re.search(r'((?:2[0-4]\d)|(?:25[0-5])|(?:1?\d?\d))\.((?:2[0-4]\d)|(?:25[0-5])|(?:1?\d?\d))\.((?:2[0-4]\d)|(?:25[0-5])|(?:1?\d?\d))\.((?:2[0-4]\d)|(?:25[0-5])|(?:1?\d?\d))', content):
 		# 69.420.69.420
 		try:
 			await message.author.send('Don\'t post IP addresses in chat, nerd')
