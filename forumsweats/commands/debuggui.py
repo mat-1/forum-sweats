31b7744a339370d3dda81e63326adeb3bb706b12
mat <27899617+mat-1@users.noreply.github.com>
2021-02-19 22:27:23 -0600

    add subguis

--- forumsweats/commands/debuggui.py
+++ forumsweats/commands/debuggui.py
@@ -1,4 +1,5 @@
-from ..gui import PaginationGUI
+import asyncio
+from ..gui import PaginationGUI, TextGUI
 
 name = 'debuggui'
 channels = ['bot-commands']
@@ -6,13 +7,14 @@ channels = ['bot-commands']
 
 async def run(message):
 	gui = PaginationGUI(
-		message.client,
-		user=message.author,
-		channel=message.channel,
 		title='Example GUI',
 		options=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	)
-	await gui.make_message()
+	await gui.make_message(
+		client=message.client,
+		user=message.author,
+		channel=message.channel,
+	)
 	# option = await gui.wait_for_option()
 
 	async for option in gui:
