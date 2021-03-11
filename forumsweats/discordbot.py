9719985152335c3ad95a6494d0afcdea36b5b1d4
mat <27899617+mat-1@users.noreply.github.com>
2021-03-11 10:05:29 -0600

    Update discordbot.py

--- forumsweats/discordbot.py
+++ forumsweats/discordbot.py
@@ -1,4 +1,3 @@
-from .commands.promoter import check_promoter
 from . import commands as commands_module
 from typing import Any, List, Union
 from .betterbot import BetterBot
@@ -209,6 +208,7 @@ async def on_member_join(member):
 
 	if used_invite:
 		await db.add_invited_member(used_invite.inviter.id, member.id)
+		from .commands.promoter import check_promoter
 		asyncio.ensure_future(check_promoter(member))
 
 	moot_end = await db.get_mooted_end(member.id)
