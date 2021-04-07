a666127b91094c33c35cd7bbf1ae5c4fd0b17cc1
mat <27899617+mat-1@users.noreply.github.com>
2021-04-07 12:48:26 -0500

    fix !promoter

--- forumsweats/commands/promoter.py
+++ forumsweats/commands/promoter.py
@@ -5,12 +5,13 @@ import discord
 import config
 
 name = 'promoter'
+aliases = ['invites']
 args = '[member]'
 
 class HasntInvitedAnyone(Exception): pass
 class UnknownInvitedMembers(Exception): pass
 
-async def check_promoter(member):
+async def check_promoter(member, inviter_string: str):
 	invited_member_ids = await db.get_invited_members(member.id)
 
 	members_activity_bobux = {}
@@ -36,26 +37,27 @@ async def check_promoter(member):
 	for member_id in members_activity_bobux:
 		member_activity_bobux = members_activity_bobux[member_id]
 		total_members_invited += 1
-		if active_members_invited > 100:
+		if member_activity_bobux >= 100:
 			active_members_invited += 1
+	
 
 	if total_members_invited == 1:
 		if active_members_invited == 1:
-			result_description = f'You\'ve invited 1 member total and they are active.'
+			result_description = f'{inviter_string} invited 1 member total and they are active.'
 		else:
-			result_description = f'You\'ve invited 1 member total but they\'re not active.'
+			result_description = f'{inviter_string} invited 1 member total but they\'re not active.'
 	else:
-		result_description = f'You\'ve invited {total_members_invited} members total and {active_members_invited} of those members are active.'
+		result_description = f'{inviter_string} invited {total_members_invited} members total and {active_members_invited} of those members are active.'
 
-	result_description += ' You need to invite 5 active members to get promoter role.'
+	result_description += ' {inviter_string} need to invite 3 active members to get promoter role.'
 
 
 	promoter_role = get_role_id(member.guild.id, 'promoter')
 	if not promoter_role:
 		result_description = 'Error: Promoter role doesn\'t exist :('
 	else:
-		if active_members_invited >= 5:
-			await member.add_roles(promoter_role, reason='Invited 5 active members')
+		if active_members_invited >= 3:
+			await member.add_roles(promoter_role, reason='Invited 3 active members')
 
 	result_embed = discord.Embed(
 		title='Promoter',
@@ -73,14 +75,19 @@ async def run(message, member: Member = None):
 	# be 100% sure the command is being run in the correct guild
 	if message.guild.id != config.main_guild:
 		return
+	
+	if member.id == message.author:
+		inviter_string = 'You'
+	else:
+		inviter_string = member.mention
 
 	result_message = await message.channel.send(embed=discord.Embed(
 		title='Promoter',
-		description='Please wait, getting activity for members you invited...'
+		description=f'Please wait, getting activity for members {inviter_string.lower()} invited...'
 	))
 
 	try:
-		embed = await check_promoter(member)
+		embed = await check_promoter(member, inviter_string)
 	except HasntInvitedAnyone:
 		return await result_message.edit(content='You haven\'t invited anyone :(', embed=None)
 	except UnknownInvitedMembers:
