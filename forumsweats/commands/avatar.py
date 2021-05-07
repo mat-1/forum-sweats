2948be5907aae3c7767488549c5f88fb57934840
mat <github@matdoes.dev>
2021-05-07 10:07:52 -0500

    make gifs png/gif

--- forumsweats/commands/avatar.py
+++ forumsweats/commands/avatar.py
@@ -4,4 +4,9 @@ name = 'avatar'
 
 async def run(message, member: Member):
 	'Gets the Discord avatar for a member'
-	await message.channel.send(str(member.avatar_url_as(format='png')))
+	await message.channel.send(str(
+		member.avatar_url_as(format='gif')
+		if member.is_avatar_animated()
+		else member.avatar_url_as(format='png')
+	))
+
