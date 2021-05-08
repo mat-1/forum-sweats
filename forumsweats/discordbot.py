2ebf7a1fb7725a9625187bb699b04e9b1d7bf1e3
mat <github@matdoes.dev>
2021-05-07 20:30:15 -0500

    bug fix: don't remove member role when a mooted member leaves and rejoins

--- forumsweats/discordbot.py
+++ forumsweats/discordbot.py
@@ -223,10 +223,6 @@ async def on_member_join(member):
 	if is_mooted:
 		moot_remaining = moot_end - time.time()
 		await moot_user(member, moot_remaining, member.guild.id, gulag_message=False)
-		await asyncio.sleep(1)
-		member_role_id = get_role_id(member.guild.id, 'member')
-		member_role = member.guild.get_role(member_role_id)
-		await member.remove_roles(member_role, reason='mee6 cringe')
 
 	mute_end = await db.get_mute_end(member.id)
 	is_muted = mute_end and mute_end > time.time()
