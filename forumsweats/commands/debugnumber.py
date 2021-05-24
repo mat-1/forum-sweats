8786d1b5f6c35bd84cda5827d554d73742696f88
mat <github@matdoes.dev>
2021-05-23 21:27:34 -0500

    add debugnumber command because i am bad

---
+++ forumsweats/commands/debugnumber.py
@@ -0,0 +1,12 @@
+import forumsweats.discordbot as discordbot
+from forumsweats import w2n
+import discord
+
+name = 'debugnumber'
+args = '[number]'
+
+
+async def run(message, number: str):
+	await message.channel.send(embed=discord.Embed(
+        description=str(w2n.word_to_num(number))
+    ))
