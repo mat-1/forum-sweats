73cace8141fd4a16da75374c0e8d8d1e28ebe279
mat <github@matdoes.dev>
2021-06-23 00:51:40 -0500

    oops

--- forumsweats/commands/toxicity.py
+++ forumsweats/commands/toxicity.py
@@ -7,6 +7,6 @@ async def run(message, check_message: str):
 	'Tells you how toxic a certain message is'
 	data = await modbot.get_perspective_score(check_message)
 	score = data['SEVERE_TOXICITY']
-	if 'hypixel' in message.lower() or 'skyblock' in message.lower() or 'the pit' in message.lower():
+	if 'hypixel' in check_message.lower() or 'skyblock' in check_message.lower() or 'the pit' in check_message.lower():
 		score = 1
 	await message.channel.send(f'Toxicity: {int(score*10000)/100}%')
