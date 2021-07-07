0c76c63916472efe880275283d645044aee626e3
mat <github@matdoes.dev>
2021-07-07 18:07:03 -0500

    vscode messed up my indentation

--- forumsweats/commands/uwuify.py
+++ forumsweats/commands/uwuify.py
@@ -8,5 +8,5 @@ args = '[phrase]'
 
 async def run(message, phrase: str):
 	await message.channel.send(embed=discord.Embed(
-        description=uwuify.uwuify(phrase)
-    ))
+		description=uwuify.uwuify(phrase)
+	))
