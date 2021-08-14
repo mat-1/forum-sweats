70887b4d18cb6665d1b187156826815ba8b22c1b
mat <github@matdoes.dev>
2021-08-14 17:00:16 -0500

    fix removesocialcredit

--- forumsweats/commands/removesocialcredit.py
+++ forumsweats/commands/removesocialcredit.py
@@ -10,7 +10,7 @@ args = '[member]'
 async def run(message, member: Member = None, amount: int = None):
 	if not member or not amount:
 		return await message.reply(f'Invalid member or amount')
-	amount = amount - 1
+	amount = - amount
 	social_credit = await db.change_social_credit(member.id, amount)
 	if amount > 0:
 		await message.channel.send(f'<@{member.id}>, you have earned **{amount}** social credit. You now have a total of {social_credit} social credit.')
