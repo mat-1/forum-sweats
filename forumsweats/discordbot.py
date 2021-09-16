e0948e9a0a365aaa9781221a7779b3ddb19aa7ba
mat <github@matdoes.dev>
2021-09-15 20:50:35 -0500

    fix bot not giving member role to mooted users who join

--- forumsweats/discordbot.py
+++ forumsweats/discordbot.py
@@ -258,19 +258,18 @@ async def on_member_join(member):
 	is_mooted = moot_end and moot_end > time.time()
 	if is_mooted:
 		moot_remaining = moot_end - time.time()
-		await moot_user(member, moot_remaining, member.guild.id, gulag_message=False)
+		asyncio.ensure_future(moot_user(member, moot_remaining, member.guild.id, gulag_message=False))
 
 	mute_end = await db.get_mute_end(member.id)
 	is_muted = mute_end and mute_end > time.time()
 	if is_muted:
 		mute_remaining = mute_end - time.time()
-		await mute_user(member, mute_remaining, member.guild.id, gulag_message=False)
+		asyncio.ensure_future(mute_user(member, mute_remaining, member.guild.id, gulag_message=False))
 		await asyncio.sleep(1)
 		member_role_id = get_role_id(member.guild.id, 'member')
 		member_role = member.guild.get_role(member_role_id)
 		await member.remove_roles(member_role, reason='mee6 cringe')
 	else:
-
 		member_role_id = get_role_id(member.guild.id, 'member')
 		member_role = member.guild.get_role(member_role_id)
 
