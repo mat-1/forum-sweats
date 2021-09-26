a0e939964b7b73276d034b7cbf038d268cedcdd1
antisynth <73140189+antisynth@users.noreply.github.com>
2021-09-26 15:44:51 -0600

    make avatar work without username (#119)
    * make avatar work without username
    
    * Update avatar.py

--- forumsweats/commands/avatar.py
+++ forumsweats/commands/avatar.py
@@ -2,8 +2,12 @@ from ..commandparser import Member
 
 name = 'avatar'
 
-async def run(message, member: Member):
+async def run(message, member: Member = None):
 	'Gets the Discord avatar for a member'
+	
+	if not member:
+		member = message.author
+		
 	await message.channel.send(str(
 		member.avatar.with_format('gif')
 		if member.avatar.is_animated()
